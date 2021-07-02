8b1adca95f26dad45ffa2b778add545b908d844c
psalguerog <pedsalga@upv.edu.es>
2021-07-02 13:56:36 +0200

    Added new palettes. Fix descriptions and examples. README created.

--- R/colorConesa_functions.R
+++ R/colorConesa_functions.R
@@ -25,18 +25,37 @@ conesa_cols <- function(...) {
 conesa_palettes <- list(
   `main`  = conesa_cols("green", "orange", "yellow"),
   `nature`= conesa_cols("green", "yellow"),
+  `sunshine`= conesa_cols("yellow", "orange"),
 
   `hot`   = conesa_cols("orange", "magenta"),
   `warm`  = conesa_cols("blue", "magenta"),
   `cold`  = conesa_cols("blue", "purple"),
 
-  `cool`  = conesa_cols("blue", "purple"),
-  `mixed` = conesa_cols("green", "orange", "yellow", "blue", "pink", "purple", "magenta")
+  `complete` = conesa_cols("green", "orange", "yellow", "blue", "pink", "purple", "magenta")
 )
+
+#' Return conesa palette
+#'
+#' Return a list of the different palettes with their colors.
+#'
+#' @export
+getConesaPalettes <- function(){
+  return(conesa_palettes)
+}
+
+
+#' Return conesa main colors
+#'
+#'Return a list of the different colors the RColorConesa packages uses.
+#'
+#' @export
+getConesaColors <- function(){
+  return(conesa_colors)
+}
 
 #' Return function to interpolate a conesa color palette
 #'
-#' @param palette Character name of palette in conesa_palettes
+#' @param palette Character name of palette in conesa_palettes: c("main", "nature", "sunshine", "hot", "warm", "cold", "complete") are avairable.
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments to pass to colorRampPalette()
 #'
@@ -54,16 +73,16 @@ conesa_pal <- function(palette = "main", reverse = FALSE, ...) {
 #'
 #' @param n Number of colors you need
 #' @param reverse Boolean indicating whether the palette should be reversed
-#' @param palette Character name of palette in conesa_palettes c("main", "cool", "cold", "warm", "hot", "nature", "mixed") are avairable.
+#' @param palette Character name of palette in conesa_palettes: c("main", "nature", "sunshine", "hot", "warm", "cold", "complete") are avairable.
 #'
 #' @examples
 #' colorSpecies <- colorConesa(3, palette = "main")
 #' plot(x = iris$Sepal.Length, y = iris$Sepal.Width, col = colorSpecies[iris$Species], pch = 16)
 #'
 #' @export
-colorConesa <- function(n, reverse = F, palette = "mixed"){
-  if(!pallete %in% names(conesa_palettes)){
-    message("Pallete musst be some of the following palettes: ", paste(names(conesa_palettes),sep=" ", collapse=", "))
+colorConesa <- function(n, reverse = F, palette = "complete"){
+  if(!palette %in% names(conesa_palettes)){
+    message("Palette musst be some of the following palettes: ", paste(names(conesa_palettes),sep=" ", collapse=", "))
   }
 
   pal <- conesa_pal(palette = palette, reverse = reverse)
@@ -72,19 +91,25 @@ colorConesa <- function(n, reverse = F, palette = "mixed"){
 
 #' Color scale constructor for conesa colors
 #'
-#' @param palette Character name of palette in conesa_palettes c("main", "cool", "warm", "hot", "nature", "mixed") are avairable.
+#' @param palette Character name of palette in conesa_palettes: c("main", "nature", "sunshine", "hot", "warm", "cold", "complete") are avairable.
 #' @param discrete Boolean indicating whether color aesthetic is discrete or not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
 #'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
 #' @examples
+#' library(ggplot2)
 #' g <- ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species))
 #' g <- g + geom_point(size = 4)
 #' g <- g + scale_color_conesa(palette = "main")
 #'
 #' @export
 scale_color_conesa <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  if(!palette %in% names(conesa_palettes)){
+    message("Palette musst be some of the following palettes: ", paste(names(conesa_palettes),sep=" ", collapse=", "))
+    stop()
+  }
+
   pal <- conesa_pal(palette = palette, reverse = reverse)
 
   if (discrete) {
@@ -96,21 +121,26 @@ scale_color_conesa <- function(palette = "main", discrete = TRUE, reverse = FALS
 
 #' Fill scale constructor for conesa colors
 #'
-#' @param palette Character name of palette in conesa_palettes c("main", "cool", "cold", "warm", "hot", "nature", "mixed") are avairable.
+#' @param palette Character name of palette in conesa_palettes c("main", "nature", "sunshine", "hot", "warm", "cold", "complete") are avairable.
 #' @param discrete Boolean indicating whether color aesthetic is discrete or not
 #' @param reverse Boolean indicating whether the palette should be reversed
 #' @param ... Additional arguments passed to discrete_scale() or
 #'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
 #'
 #' @examples
-#' mpg <- ggplot2::mpg
+#' library(ggplot2)
 #' g <- ggplot(mpg, aes(manufacturer, fill = manufacturer))
 #' g <- g + geom_bar()
 #' g <- g + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-#' g <- g + scale_fill_conesa(palette = "mixed")
+#' g <- g + scale_fill_conesa(palette = "complete")
 #'
 #' @export
 scale_fill_conesa <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  if(!palette %in% names(conesa_palettes)){
+    message("Palette musst be some of the following palettes: ", paste(names(conesa_palettes),sep=" ", collapse=", "))
+    stop()
+  }
+
   pal <- conesa_pal(palette = palette, reverse = reverse)
 
   if(discrete){
---
+++ README.md
@@ -0,0 +1,138 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# RColorConesa
+
+## Installation
+
+You can install the released version of RColorConesa from GitHub with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("ConesaLab/RColorConesa")
+```
+
+## Information
+
+The package RColorConesa is a color packages with different palletes
+inspired by the corporative image of the Conesa Lab.
+
+![Logo ConesaLab](./images/logo_ConesaLab.png)
+
+## Palettes
+
+RColorConesa consists of seven main colours from which a number of
+different colour palettes have been created.
+
+## Palette - main
+
+<img src="man/figures/README-palette1-1.png" width="100%" />
+
+## Palette - nature
+
+<img src="man/figures/README-palette2-1.png" width="100%" />
+
+## Palette - sunshine
+
+<img src="man/figures/README-palette3-1.png" width="100%" />
+
+## Palette - hot
+
+<img src="man/figures/README-palette4-1.png" width="100%" />
+
+## Palette - warm
+
+<img src="man/figures/README-palette5-1.png" width="100%" />
+
+## Palette - cold
+
+<img src="man/figures/README-palette6-1.png" width="100%" />
+
+## Palette - complete
+
+<img src="man/figures/README-palette7-1.png" width="100%" />
+
+## Example - plot()
+
+This is a basic example which shows you how to solve a common problem
+using the databases “iris” and the basic ploting function in R. The
+objetive is color by spsecie using the RcolorConesa packages.
+
+To do this, we can use the funtion “colorConesa()” which returns a list
+of as many colors of you needed.
+
+``` r
+library(RColorConesa)
+n_species <- length(levels(iris$Species))
+colorSpecies <- colorConesa(n_species, palette = "main")
+plot(x = iris$Sepal.Length, y = iris$Sepal.Width, col = colorSpecies[iris$Species], pch = 16)
+legend("bottomleft", legend=c(levels(iris$Species)), col=colorSpecies, lty=1)
+```
+
+<img src="man/figures/README-example plot-1.png" width="100%" />
+
+## Example - ggplot()
+
+In case of ggplot(), if we want to plot by color it is much more easier
+to do with the functions “scale\_color\_conesa()” and
+“scale\_fill\_conesa()”. We plot the same result we made before.
+
+``` r
+library(RColorConesa)
+library(ggplot2)
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  geom_point(size = 4) +
+  scale_color_conesa(palette = "main")
+```
+
+<img src="man/figures/README-example ggplot-1.png" width="100%" />
+
+A good point for the packages is you can use more colors that it already
+have. For example, we are going to color each Manufacturer from the
+dataset “mpg” from the package “ggplot2”.
+
+As you can see, the palette “complete” only have 7 colors, but it can
+interpolate as much as need it to fill all the categories.
+
+``` r
+library(RColorConesa)
+library(ggplot2)
+
+mpg <- ggplot2::mpg
+
+ggplot(mpg, aes(manufacturer, fill = manufacturer)) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_conesa(palette = "complete")
+```
+
+<img src="man/figures/README-example ggplot - fill-1.png" width="100%" />
+
+Finally, for continuos variables, you can change the parameter discrete
+to False in the functions “scale\_fill\_conesa()” and
+“scale\_fill\_conesa()”.
+
+``` r
+library(RColorConesa)
+library(ggplot2)
+
+df.heatmap <- expand.grid(Var1 = letters[1:15], Var2 = 1:15)
+df.heatmap$score <- runif(nrow(df.heatmap), min = -5, max = 5)
+
+ggplot(df.heatmap, aes(x = Var1, y = Var2, fill = score)) + 
+  geom_tile() + 
+  scale_fill_conesa(palette = "sunshine", discrete = F)
+```
+
+<img src="man/figures/README-example - continuous-1.png" width="100%" />
+
+# Contact
+
+Note that RColorConesa can be updated. If you encounter a problem,
+please open an issue via GitHub or send an email to
+<pedsalga@upv.edu.es>.
+
+# Author
+
+Pedro Salguero García - <pedsalga@upv.edu.es>
